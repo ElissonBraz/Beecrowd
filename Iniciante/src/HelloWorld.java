61134a139f5b6a4a62b738746c589438fc759648
Elisson Braz <59119081+ElissonBraz@users.noreply.github.com>
2025-04-16 22:49:30 -0300

    Update HelloWorld.java

--- Iniciante/src/HelloWorld.java
+++ Iniciante/src/HelloWorld.java
@@ -1,3 +1,13 @@
+/* Bem-vindo ao beecrowd! 
+ * 
+ * O seu primeiro programa em qualquer linguagem de programação normalmente é o "HELLO WORLD!".
+ * Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+ * 
+ * Entrada: NULL
+ * 
+ * Saida: Você deve imprimir a mensagem "Hello, World!".
+*/
+
 public class HelloWorld {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
